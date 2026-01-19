5c3d4875e077c96db9a64ea423919a081785c350
unknown <bhardwajdev269@gmail.com>
2026-01-19 23:36:22 +0530

    updated hearder files

--- enemy.cs
+++ enemy.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    // Enemy for Map 1 (lv 1 to 20)
     public int level = 100;
 
     [Header("Health")]
-    public int maxHealth = 1000;
-    public int currentHealth = 1000;
+    private int maxHealth = 1000;
+    private int currentHealth = 1000;
+
 
     [Header("Shield")]
-    public float shieldPercent = 0.3f;   // 30%
-    public int maxShield;
-    public int currentShield;
+    private float shieldPercent = 0.3f;   // 30%
+    private int maxShield;
+    private int currentShield;
 
   void Start()
     {
@@ -24,7 +26,6 @@ public class Enemy : MonoBehaviour
         Debug.Log($"Enemy shield init: maxShield={maxShield}, currentShield={currentShield}");
     }
 
-
     public void TakeDamage(int amount)
 {
     // damage shield first
@@ -33,7 +34,7 @@ public class Enemy : MonoBehaviour
         int shieldDamage = Mathf.Min(amount, currentShield);
         currentShield -= shieldDamage;
         amount -= shieldDamage;
-
+        
         if (currentShield <= 0)
         {
             EnemyUI.Instance?.OnShieldBroken();
